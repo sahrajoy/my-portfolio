f8b71fd35189ccd77c88710aef67052002d2169d
sahrajoy <sahra.stursa@live.at>
2023-11-02 19:29:35 +0100

    add nav and swiper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA,  NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -29,6 +29,7 @@ import { FooterComponent } from './footer/footer.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
+  clickCounter: number = 0;
+  @ViewChild('nav') myDiv!: ElementRef<HTMLElement>;
 
+  ngAfterViewInit(): void {
+    // You can be sure that myDiv is available here if you need to do initial setup
+  }
+  
+  myFunction(): void {
+    this.clickCounter++;
+    if (this.clickCounter % 2 === 1) { 
+      this.myDiv.nativeElement.style.display = 'block';
+    } else {
+      this.myDiv.nativeElement.style.display = 'none';
+    }
+  }
+
+  navVisible = false;
+
+  toggleNav() {
+    this.navVisible = !this.navVisible;
+  }
 }
